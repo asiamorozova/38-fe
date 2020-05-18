d3e5764e448f2e13f4f73765746b18880b83beaa
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-18 15:02:45 -0700

    added store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
+import store from '../src/store';
 
 render(
   <Provider store={store}>
